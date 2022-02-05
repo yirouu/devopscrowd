a0cf7015b5d808fe7729d09357371c8c9c3c18e7
fooxinyitasmin <2004219H@student.tp.edu.sg>
2022-02-05 21:44:54 +0800

    Added Cart class that extends ProductView along with quantity attribute

---
+++ devopscrowd/src/main/java/Cart.java
@@ -0,0 +1,15 @@
+
+public class Cart extends ProductView {
+	private int quantity;
+
+	public Cart() {
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+}
