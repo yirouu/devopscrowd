29587a30d93d1b5e06a0b9759a9d78c12140be08
fooxinyitasmin <2004219H@student.tp.edu.sg>
2022-02-24 18:27:02 +0800

    added orderCollectionTest for unit testing

---
+++ src/test/java/OrderCollectionTest.java
@@ -0,0 +1,87 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class OrderCollectionTest {
+	private OrderCollection oc;
+	private Orders o1;
+	private Orders o2;
+	private Orders o3;
+	private Orders o4;
+	private Orders o5;
+	private final int ORDERS_SIZE = 4;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		oc = new OrderCollection();
+		o1 = new Orders(001, "2022-01-28 16:50:47", 001, 001, "pending", "product1", 0, "image1", "user1", "email1",
+				"address1", 001);
+		o2 = new Orders(002, "2022-01-28 16:50:47", 002, 002, "pending", "product2", 0, "image2", "user2", "email2",
+				"address2", 002);
+		o3 = new Orders(003, "2022-01-28 16:50:47", 003, 003, "pending", "product3", 0, "image3", "user3", "email3",
+				"address3", 003);
+		o4 = new Orders(004, "2022-01-28 16:50:47", 004, 004, "pending", "product4", 0, "image4", "user4", "email4",
+				"address4", 004);
+		o5 = new Orders(005, "2022-01-28 16:50:47", 005, 005, "pending", "product5", 0, "image5", "user5", "email5",
+				"address5", 005);
+		oc.add(o1);
+		oc.add(o2);
+		oc.add(o3);
+		oc.add(o4);
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testGetOrders() {
+		oc.getOrders();
+	}
+
+	@Test
+	void testAdd() {
+		int capacity = 20;
+		List<Orders> testOrders = oc.getOrders();
+		assertEquals(testOrders.size(), ORDERS_SIZE);
+		oc.add(o5);
+	}
+
+	@Test
+	void testFindOrdersById() {
+		int id = 2;
+		oc.findOrdersById(id);
+		oc.findOrdersById(0);
+	}
+
+	@Test
+	void testEditOrder() {
+		int orderid = 001;
+		String orderDateTime = "2022-01-28 16:50:47";
+		int orderUserId = 01;
+		int productId = 01;
+		String orderStatus = "approved";
+		String name = "product";
+		int price = 10;
+		String image = "image";
+		String username = "username";
+		String email = "email";
+		String address = "address";
+		int postal = 01;
+
+		oc.editOrder(orderid, orderDateTime, orderUserId, productId, orderStatus, name, price, image, username, email,
+				address, postal);
+		
+	}
+
+	@Test
+	void testDeleteOrder() {
+		int id = 003;
+		oc.deleteOrder(id);
+	}
+
+}
